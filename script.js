e07ee060bc7590bcbb570e6c3bc4ec2cbc26a10c
Christophe Coquelet <coquelet.c@gmail.com>
2016-04-25 23:23:12 +0200

    remove old code && add click to option

--- script.js
+++ script.js
@@ -1,23 +1,29 @@
 document.addEventListener("DOMContentLoaded", function(event) { 
 
-  var video = document.getElementById("videoSample");
-  // var chapters = textTracks.cues;
-  // console.log(textTracks);
-  // console.log(textTracks.getCueById("Chapter 1"));
-  // 
-  document.querySelector('track').addEventListener('load', function () {
-            var textTracks = video.textTracks[0],
-                chapters   = textTracks.cues,
-                list       = document.querySelector('#chapterList');
-            
-            list.seize = chapters.length < 5 ? chapters.length : 5;
-            
-            for (i = 0; i < chapters.length; i++) {
-                console.log(chapters[i]);
-                var option = document.createElement('option');
-                option.text = chapters[i].id;
-                list.appendChild(option);
-                console.log(chapters[i].endTime);
-            }
-  })
+	var video = document.getElementById("videoSample");
+	
+	document.querySelector('track').addEventListener('load', function () {
+						var textTracks = video.textTracks[0],
+								chapters   = textTracks.cues,
+								list       = document.querySelector('#chapterList'),
+								i;
+						
+						
+						var optionClick = function (e) {
+								var start = e.target.dataset.start;
+								video.currentTime = start;
+						};
+						
+						list.seize = chapters.length < 5 ? chapters.length : 5;
+						
+						for (i = 0; i < chapters.length; i++) {
+								console.log(chapters[i]);
+								var option = document.createElement('option');
+								option.setAttribute('data-start', chapters[i].startTime);
+								option.text = chapters[i].id;
+								option.onclick = optionClick;
+								list.appendChild(option);
+								console.log(chapters[i].endTime);
+						}
+	});
 });
