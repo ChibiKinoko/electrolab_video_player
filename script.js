dff5313ce0cebe7ca1a2f6368f66ffe97872e1df
laureM <laure.meng@gmail.com>
2016-04-27 18:18:17 +0200

    synchro video chapters OK

--- script.js
+++ script.js
@@ -27,6 +27,9 @@ document.addEventListener("DOMContentLoaded", function(event) {
 			}
 		};
 
+
+		
+
 		// console.log(subtitles);
 		// console.log(chapters);
 
@@ -37,6 +40,7 @@ document.addEventListener("DOMContentLoaded", function(event) {
 			var chapterElem = document.createElement("li");
 			chapterElem.innerHTML = ' - ' + chapters[i].id;
 			chapterElem.setAttribute('data-start', chapters[i].startTime);
+			chapterElem.setAttribute('data-end', chapters[i].endTime);
 			chaptersList.appendChild(chapterElem);
 
 			chapterElem.addEventListener('click', function () {
@@ -57,6 +61,25 @@ document.addEventListener("DOMContentLoaded", function(event) {
 				optSRT.setAttribute('data-show', 'showing');
 			}
 			document.getElementById('subtitles').appendChild(optSRT);
+		};
+
+		// console.log(document.querySelector('\[data-start="' + 0 + '"]'));
+
+		video.ontimeupdate = function (e) {
+			
+			// console.log(Math.round(video.currentTime));
+
+			for (var c = 0; c < chapters.length; c++) {
+				var myData = document.querySelector('\[data-start="' + chapters[c].startTime + '"]');
+				if (video.currentTime >= chapters[c].startTime && video.currentTime <= chapters[c].endTime && myData.className.split(' ').indexOf("selected") === -1) {
+					// console.log('current');
+					myData.className += ' selected';
+					if (c !== 0) {
+						myData.previousSibling.classList.remove('selected');
+					};
+					break;
+				};
+			};
 		};
 	});
 
