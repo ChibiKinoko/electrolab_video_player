2d3b89af4ed00418fe6b28d6720a86a5cfcbe977
laureM <laure.meng@gmail.com>
2016-05-24 15:19:02 +0200

    add Json data for timestamp, parse it

--- script.js
+++ script.js
@@ -2,7 +2,49 @@ document.addEventListener("DOMContentLoaded", function(event) {
 
 	var video = document.getElementById("videoSample");
 	var subtitles = [];
+	var timestamp;
+	var currentPdfPage = 1;
+	var alwaysDisplay = false;
 
+	/*
+	*
+	* Initialize by getting Timestamp Json data
+	* 
+	 */
+	function init()
+	{
+		var getTimestamp = new Promise(function(resolve, reject) {
+			var req = new XMLHttpRequest();
+			
+			req.open('GET', 'videos/Timestamp.json', true);
+			
+			req.onreadystatechange = function () {
+			  	if (req.readyState == 4) {
+			     	if(req.status == 200)
+			     		resolve(JSON.parse(req.responseText));
+			     	else
+			      		reject('Could not get timestamp.json');
+			  	}
+			};
+
+			req.send(null);
+		});
+
+		getTimestamp.then(function(data) {
+			// console.log(data);
+			main(data);
+		}, function(error) {
+			// console.log(error);
+			main();
+		});
+	}
+
+
+	/*
+	*
+	* Switch with the given subtitle
+	* 
+	 */
 	function toggleSub(lang) {
 		for (var a = 0; a < video.textTracks.length; a++) {
 			if (video.textTracks[a].kind === 'subtitles') {
@@ -13,99 +55,190 @@ document.addEventListener("DOMContentLoaded", function(event) {
 				}
 			}
 		};
-
-		// console.log(subtitles);
 	}
 
-	document.querySelector('track').addEventListener('load', function () {
-
-		for (var a = 0; a < video.textTracks.length; a++) {
-			if (video.textTracks[a].kind === 'subtitles') {
-				subtitles.push(video.textTracks[a]);
-			} else {
-				var chapters = video.textTracks[a].cues;
-			}
-		};
-
-		// console.log(subtitles);
-		// console.log(chapters);
-
+	/*
+	*
+	* Get Chapters and display in DOM
+	* 
+	 */	
+	function displayChapters(chapters)
+	{
 		var chaptersList = document.createElement("ul");
-		document.getElementById('chapters').appendChild(chaptersList);
+			document.getElementById('chapters').appendChild(chaptersList);
 
-		for (i = 0; i < chapters.length; i++) {
-			var chapterElem = document.createElement("li");
-			chapterElem.innerHTML = ' - ' + chapters[i].id;
-			chapterElem.setAttribute('data-start', chapters[i].startTime);
-			chapterElem.setAttribute('data-end', chapters[i].endTime);
-			chaptersList.appendChild(chapterElem);
+			for (i = 0; i < chapters.length; i++) {
+				var chapterElem = document.createElement("li");
+				chapterElem.innerHTML = ' - ' + chapters[i].id;
+				chapterElem.setAttribute('data-start', chapters[i].startTime);
+				chapterElem.setAttribute('data-end', chapters[i].endTime);
+				chaptersList.appendChild(chapterElem);
 
-			chapterElem.addEventListener('click', function () {
-				video.currentTime = this.dataset.start;
-  				if(video.paused) { 
-  					video.play();
-  				}
-			})
-		}
+				chapterElem.addEventListener('click', function () {
+					video.currentTime = this.dataset.start;
+	  				if(video.paused) { 
+	  					video.play();
+	  				}
+				})
+			}
+	}
 
+	/*
+	*
+	* Get subtitles and diplay in DOM select
+	*
+	 */
+	function displaySubtitle(subtitles)
+	{
 		for (var j = 0; j < subtitles.length; j++) {
 			var optSRT = document.createElement("option");
 			optSRT.innerHTML = subtitles[j].language;
 
-			if (optSRT.id !== "default") {
+			// console.log(subtitles[j].id); 
+
+			if (subtitles[j].id !== "default") {
 				optSRT.setAttribute('data-show', 'hidden');
 			} else {
 				optSRT.setAttribute('data-show', 'showing');
 			}
 			document.getElementById('subtitles').appendChild(optSRT);
 		};
+	}
 
-		// console.log(document.querySelector('\[data-start="' + 0 + '"]'));
+	function convert(time)
+	{
+		var seconds = Math.round(time);
+		var hours = parseInt( seconds / 3600 );
+		var minutes = parseInt( seconds / 60 );
+		// var seconds = seconds % 60;
 
-		video.ontimeupdate = function (e) {
+		var result = (hours < 10 ? "0" + hours : hours) + ":" + (minutes < 10 ? "0" + minutes : minutes) + ":" + (seconds  < 10 ? "0" + seconds : seconds);
+		return result;
+	}
+
+	function displayPdf(page)
+	{
+		var canvas = document.getElementById('canvasSlide');
+		var context = canvas.getContext('2d');
+
+		PDFJS.getDocument('cahier_des_charges.pdf').then(function (pdf) {
+			pdf.getPage(page).then(function (pageDisplay) {
+
+				var viewport = pageDisplay.getViewport(1);
+				viewport.height = canvas.height;
+				viewport.width = canvas.width;
+
+				var renderContext = {
+					canvasContext: context,
+					viewport: viewport
+				};
+
+				pageDisplay.render(renderContext);
+			});
+		});
+	}
+
+	function synchronise(timestamp) {
+		// console.log(timestamp);
+
+		for(index in timestamp) {
+			// console.log(timestamp[index].end);
+			if (convert(video.currentTime) > timestamp[index].start && convert(video.currentTime) < timestamp[index].end) {
+				// console.log(typeof parseInt(index, 10));
+				if (parseInt(index, 10) !== currentPdfPage || !alwaysDisplay) {
+					alwaysDisplay = true;
+					currentPdfPage = parseInt(index, 10);
+					displayPdf(parseInt(index, 10));
+				}
+			};
+			// console.log(video.currentTime);
+		}
+		// console.log(convert(video.currentTime));
+	}
+
+	function main(timestamp) {
+		timestamp = timestamp || null;
+
+		if (timestamp == null ) {
+			var newItem = document.createElement("p");
+			var textnode = document.createTextNode("Synchronisation Off");
+			newItem.appendChild(textnode);
+
+			var list = document.getElementById("container");
+
+			list.insertBefore(newItem, list.childNodes[0]);
+		};
+
+		/*
+		*
+		* If track isn't load, cannot use it
+		* 
+		 */	
+		document.querySelector('track').addEventListener('load', function () {
+
+			/*
+			*
+			* Listen on the subtitle select
+			*
+			 */	
+			document.getElementById('subtitles').addEventListener('change', function(e) {
+				toggleSub(e.target.options[e.target['selectedIndex']].value);
+			});
+
+			// separate chapters from subtitle because it's both tracks html element
+			for (var a = 0; a < video.textTracks.length; a++) {
+				if (video.textTracks[a].kind === 'subtitles') {
+					subtitles.push(video.textTracks[a]);
+				} else {
+					var chapters = video.textTracks[a].cues;
+				}
+			};
+
+			// console.log(subtitles);
+			// console.log(chapters);
+
+			displayChapters(chapters);
 			
-			// console.log(Math.round(video.currentTime));
+			displaySubtitle(subtitles);
 
-			for (var c = 0; c < chapters.length; c++) {
-				var myData = document.querySelector('\[data-start="' + chapters[c].startTime + '"]');
-				if (video.currentTime >= chapters[c].startTime && video.currentTime <= chapters[c].endTime && myData.className.split(' ').indexOf("selected") === -1) {
-					// console.log('current');
-					
-					var previousSelected = document.querySelectorAll('.selected');
+			displayPdf(currentPdfPage);
 
-					for (var p = 0; p < previousSelected.length; p++) {
-						console.log(previousSelected[p]);
-						previousSelected[p].classList.remove('selected');
+
+			/**
+			 * 
+			 * Listen video change and update chapters and slide
+			 * 			 
+			*/
+			video.ontimeupdate = function (e) {
+				
+				// console.log(Math.round(video.currentTime));
+
+				for (var c = 0; c < chapters.length; c++) {
+					var myData = document.querySelector('\[data-start="' + chapters[c].startTime + '"]');
+					if (video.currentTime >= chapters[c].startTime && video.currentTime <= chapters[c].endTime && myData.className.split(' ').indexOf("selected") === -1) {
+						// console.log('current');
+						
+						var previousSelected = document.querySelectorAll('.selected');
+
+						for (var p = 0; p < previousSelected.length; p++) {
+							// console.log(previousSelected[p]);
+							previousSelected[p].classList.remove('selected');
+						};
+						myData.className += ' selected';
+						break;
+		
 					};
-					myData.className += ' selected';
-					break;
+				};
+
+
+				if (timestamp !== null) {
+					synchronise(timestamp);
 				};
 			};
-		};
-	});
+		});
+	}
+	
 
-	var canvas = document.getElementById('canvasSlide');
-	var context = canvas.getContext('2d');
-
-	PDFJS.getDocument('cahier_des_charges.pdf').then(function (pdf) {
-		pdf.getPage(1).then(function (page) {
-
-			var viewport = page.getViewport(1);
-			viewport.height = canvas.height;
-			viewport.width = canvas.width;
-
-			var renderContext = {
-				canvasContext: context,
-				viewport: viewport
-			};
-
-			// console.log(page);
-			page.render(renderContext);
-		})
-	});
-
-	document.getElementById('subtitles').addEventListener('change', function(e) {
-		toggleSub(e.target.options[e.target['selectedIndex']].value);
-	})
+	init();
 
 });
